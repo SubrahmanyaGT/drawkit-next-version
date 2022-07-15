a2b07ec333e7e0fc0cace898e8d9441129d34932
Subrahmanya G T <subrahmanyagt@gmail.com>
2022-07-15 11:34:23 +0530

    verification-redirect

--- pages/verification.js
+++ pages/verification.js
@@ -86,7 +86,7 @@ export default function Signin(props) {
             verificationResult.user.id
           )
         );
-        // router.push('/choose-subscription')
+        router.push('/choose-subscription')
       }
     }
   }
